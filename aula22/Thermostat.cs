f069a5474d8666930963e8642459ea777628a571
Jose Simao <jsimao@cc.isel.ipl.pt>
2014-12-02 18:29:54 +0000

    Aula 22

---
+++ aula22/Thermostat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab2
+{
+    public class EventArgsThermostat : EventArgs
+    {
+        private int prev, curr;
+        public EventArgsThermostat(int prev, int curr)
+        {
+            this.prev = prev;
+            this.curr = curr;
+        }
+        public int PrevTemperature { get { return prev; } }
+        public int CurrTemperature { get { return curr; } }
+    }
+
+    public class Thermostat
+    {
+        protected int currTemperature;
+        public event EventHandler TemperatureChanged;
+        public int Temperature
+        {
+            get { return currTemperature; }
+            set
+            {
+                if (TemperatureChanged != null) 
+                    TemperatureChanged(this, new EventArgsThermostat(currTemperature, value));
+                currTemperature = value;
+            }
+        }
+    }
+}
