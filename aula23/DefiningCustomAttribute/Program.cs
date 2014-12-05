075a68f1a19961692412889668da7e87a768b923
Jose Simao <jsimao@cc.isel.ipl.pt>
2014-12-05 09:14:00 +0000

    [Aula 23]: Custom attributes

---
+++ aula23/DefiningCustomAttribute/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public enum Color { Red }
+
+
+namespace DefiningCustomAttribute
+{
+
+    [AttributeUsage(AttributeTargets.All,AllowMultiple=true)]
+    public sealed class TesterAttribute : Attribute
+    {
+        public TesterAttribute(String Name)
+        {
+            this.ClassName = ClassName;
+            this.Name = Name;
+            this.DateOfTest = DateOfTest;
+        }
+
+        public string ClassName { get; set; }
+        public string Name { get; set; }
+        public string DateOfTest { get; set; }
+    }
+
+    [Tester("user2")]
+    [Tester("user1", ClassName="SomeType", DateOfTest="1-1-1990")]
+    public sealed class SomeType
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(SomeType);
+            Console.WriteLine(t.IsDefined(typeof(TesterAttribute), false));
+            TesterAttribute[] ta = 
+                (TesterAttribute[]) t.GetCustomAttributes(typeof(TesterAttribute), false);
+            foreach (TesterAttribute a in ta) {
+                Console.WriteLine(a.Name);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ aula23/UsingCustomAttributes/Using.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// [Flags]
+// [Serializable]
+[Flags, Serializable] // Equivalente às duas linhas acima.
+enum Actions
+{
+    None = 0x01,
+    Read = 0x02,
+    Write = 0x04,
+    ReadWrite = Read | Write,
+    Delete = 0x08,
+    Query = 0x10
+}
+
+class Sample
+{
+    [DllImport("user32.dll")]
+    public static extern int MessageBoxA(int p, string m, string h, int t);
+
+    public static void Main()
+    {
+        Actions ac = Actions.Write | Actions.Query;
+        Console.WriteLine(ac);
+
+        MessageBoxA(0, "Hello World!", "DLLImport Sample", 0);
+
+        System.Windows.Forms.MessageBox.Show("Hello World!", "Windows Forms Sample");
+    }
+}
