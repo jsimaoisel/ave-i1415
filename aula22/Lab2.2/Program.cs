9ede2a3ba21209a4014208bf7c748d5afa67218f
Jose Simao <jsimao@cc.isel.ipl.pt>
2014-12-05 09:04:35 +0000

    [Aula 22] Laboratorio 2 - alíneas 2.2 e 3

---
+++ aula22/Lab2.2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab2
+{
+    public class EventArgsThermostat : EventArgs
+    {
+        private int prev, curr;
+        public EventArgsThermostat(int prev, int curr)
+        {
+            this.prev = prev;
+            this.curr = curr;
+        }
+        public int PrevTemperature { get { return prev; } }
+        public int CurrTemperature { get { return curr; } }
+    }
+
+    public class Thermostat
+    {
+        protected int currTemperature;
+        public event EventHandler TemperatureChanged;
+        public int Temperature
+        {
+            get { return currTemperature; }
+            set
+            {
+                if (TemperatureChanged != null) 
+                    TemperatureChanged(this, new EventArgsThermostat(currTemperature, value));
+                currTemperature = value;
+            }
+        }
+
+        public static void M(object sender, EventArgs eventArgs)
+        {
+            EventArgsThermostat arg = eventArgs as EventArgsThermostat;
+            Console.WriteLine("Prev={0} Curr={1}", arg.PrevTemperature, arg.CurrTemperature);
+        }
+
+        public static void Main(String[] args)
+        {
+            Thermostat thermo = new Thermostat();
+
+            thermo.TemperatureChanged += M;
+
+
+
+            Assembly assembly = Assembly.LoadFrom("Lab2.2.exe");
+            foreach(Type t in assembly.GetTypes()) 
+            {
+                foreach (MethodInfo mi in t.GetMethods())
+                {
+                    // devia usar IsCompatible(typeof(EventHandler), mi)
+                    if (mi.Name == "M")
+                    { 
+                        EventHandler eh = 
+                            (EventHandler)Delegate.CreateDelegate(typeof(EventHandler), mi);
+
+                        // OR
+
+                        EventHandler eh2 =
+                            (sender, eargs) =>
+                            {
+                                mi.Invoke(null,
+                                new object[] { 
+                                    sender, eargs
+                                });
+                            };
+
+
+                        thermo.TemperatureChanged += eh;
+                    }
+                }
+            }
+
+            thermo.Temperature = 10;
+        }
+    }
+}
---
+++ aula22/Lab3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace dynamic_events
+{
+   public class Observer 
+    {
+        public void MyObserver(Object sender, EventArgs args)
+        {
+            Console.WriteLine("MyObserver called by {0}", sender);
+        }
+    }
+
+    public class MyClass
+    {
+        public event EventHandler MyEvent;
+
+        public void CallObservers()
+        {
+            if (MyEvent != null)
+                MyEvent(this, null);
+        }
+    }
+
+    public class Program
+    {
+        public static void RegisterObserver(Object target, EventInfo ev, Object delObj, MethodInfo mi)
+        {
+            ev.AddEventHandler(
+                target, 
+                Delegate.CreateDelegate(ev.EventHandlerType, delObj, mi)
+            );
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass p = new MyClass();
+            Observer delObj = new Observer();
+            RegisterObserver(
+                p, 
+                typeof(MyClass).GetEvent("MyEvent"), 
+                delObj,
+                typeof(Observer).GetMethod("MyObserver"));
+            p.CallObservers();
+        }
+    }
+}
+
+
+
+
+
+
